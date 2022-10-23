e4c99a80189fb0918f8cd143f2f399afdd330db5
winston-0 <109471877+winston-0@users.noreply.github.com>
2022-10-24 01:42:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,43 @@ const autoComplete = document.querySelector('.search-container__autocompete-list
 const reposList = document.querySelector('.pinned-repos__list');
 const searchContainer = document.querySelector('.search-container')
 
-searchBar.oninput = async function(e) {
-  let valueForSearch = searchBar.value;
-  if(valueForSearch.trim() === '') {
-    autoComplete.innerHTML = '';
-  } 
-    let items = await getRepos(valueForSearch);
-    if(items === "") {
-      return
-    }
-     autoComplete.innerHTML = '';
-     for(let i of items) {
-        renderAutoComplete(i);
-     } 
+
+
+searchBar.oninput = async () => {
+  if(searchBar.value.trim() === "") {
+    autoComplete.innerHTML = "";
+  } else {
+  let repos = await getItems(searchBar.value);
+  let items = repos.items;
+    items.forEach(i => {
+      renderAutoComplete(i);
+    })
+  }
+} 
+
+
+async function sendRequest(value) {
+  let response = await fetch(`https://api.github.com/search/repositories?q=${value}&per_page=5&page=1`);
+  return await response.json();
 }
+
+function debounceRequest() {
+  let timerId = null;
+  return function(searchValue) {
+    if(searchValue.trim() === "") {
+      clearTimeout(timerId);
+    } else {
+      return new Promise((resolve) => {
+         clearTimeout(timerId);
+          timerId = setTimeout( () => {
+            resolve(sendRequest(searchValue));
+          }, 1200);
+      })
+  }
+}
+}
+
+let getItems = debounceRequest();
 
 autoComplete.onclick = (e) => {
   pinRepo(e.target);
@@ -28,50 +51,30 @@ reposList.onclick = (e) => {
   }
 }
 
- function getData() {
-  let timerId = null;
-  return function(value) {
-    return new Promise((resolve, reject) => {
-      if(value.trim() === "") {
-        clearTimeout(timerId);
-        return resolve('')
-      } 
-      clearTimeout(timerId);
-      timerId = setTimeout( async function() {
-        let response = await fetch(`https://api.github.com/search/repositories?q=${value}&per_page=5&page=1`);
-        let data = await response.json();
-         resolve(data.items)
-      }, 1200)
-  }) 
-  }
-}
-let getRepos = getData();
 
 function renderAutoComplete(repo) {
-   let autoCompleteItem = document.createElement('li')
-   autoCompleteItem.classList.add('search-container__autocompete-list-item');
-   autoCompleteItem.innerHTML = repo.name;
-   autoCompleteItem.setAttribute('data-name', repo.name)
-   autoCompleteItem.setAttribute('data-owner', repo.owner.login);
-   autoCompleteItem.setAttribute('data-stars', repo.stargazers_count);
-   autoComplete.append(autoCompleteItem)
+  let autoCompleteItem =  `
+ <li> 
+  <button class="search-container__autocompete-list-item"
+  data-stars = "${repo.stargazers_count}"
+  data-name = "${repo.name}"
+  data-owner = "${repo.owner.login}">
+  ${repo.name}</button>
+ </li>
+  `;
+  autoComplete.insertAdjacentHTML('afterbegin', autoCompleteItem)
 }
 
 function pinRepo(repo) {
-  const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
-  let reposListItem = document.createElement('li');
-  reposListItem.classList.add('pinned-repos__list-item');
-  let listOfRepoData = document.createElement('ul');
-  listOfRepoData.classList.add('pinned-repos__list-item-data', 'list')
-  let dataAttr = repo.dataset;
-  for(let i in dataAttr) {
-    let data = document.createElement('li');
-    data.innerHTML = `${capitalize(i)}: ${dataAttr[i]}`;
-    listOfRepoData.append(data);
-  }
-  let closeButton = document.createElement('button');
-  closeButton.classList.add('pinnned-repos__list-button');
-  reposListItem.append(listOfRepoData);
-  reposListItem.append(closeButton);
-  reposList.append(reposListItem)
+  let reposListItem = `
+  <li class = "pinned-repos__list-item">
+    <ul class = "pinned-repos__list-item-data list">
+      <li>Name: ${repo.dataset.name}</li>
+      <li>Owner: ${repo.dataset.owner}</li>
+      <li>Stars: ${repo.dataset.stars}</li>
+    </ul>
+    <button class = "pinnned-repos__list-button"></button>
+  </li>
+  `;
+  reposList.insertAdjacentHTML('afterbegin', reposListItem)
 }
